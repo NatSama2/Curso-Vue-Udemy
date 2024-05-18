580d601533f06312a51acddd484b080b6d416580
NatSama2 <vet.nat.c@gmail.com>
2024-05-18 16:39:30 -0400

    logica de los nombres

--- 04-pokemon-game/src/components/PokemonPicture.vue
+++ 04-pokemon-game/src/components/PokemonPicture.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="pokemon-container">
-    <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-      class="hidden-pokemon"
-      alt="Pokemon"
-    />
-    <!--
-    <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-      class="fade-in"
-      alt="Pokemon"
-    /> -->
+    <img :src="imgSrc" class="hidden-pokemon" alt="Pokemon" />
+
+    <img v-if="showPokemon" :src="imgSrc" class="fade-in" alt="Pokemon" />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true,
+    },
+    showPokemon: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  computed: {
+    imgSrc() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+    },
+  },
+};
 </script>
 
 <style scoped>
--- 04-pokemon-game/src/pages/PokemonPage.vue
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -2,7 +2,7 @@
   <h1>¿Quien es este pokémon?</h1>
 
   <!--TODO img-->
-  <PokemonPicture />
+  <PokemonPicture :pokemonId="4" :showPokemon="true" />
 
   <!--Todo Opciones-->
 
@@ -12,6 +12,10 @@
 <script>
 import PokemonPicture from "../components/PokemonPicture";
 import PokemonOptions from "../components/PokemonOptions";
+
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+console.log(getPokemonOptions());
 
 export default {
   name: "PokemonPage",
