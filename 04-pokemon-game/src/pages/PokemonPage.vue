7994f592945fc9c6230322237220f82f1f229924
NatSama2 <vet.nat.c@gmail.com>
2024-05-18 01:46:25 -0400

    Diseño de los componentes

---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>¿Quien es este pokémon?</h1>
+
+  <!--TODO img-->
+  <PokemonPicture />
+
+  <!--Todo Opciones-->
+
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from "../components/PokemonPicture";
+import PokemonOptions from "../components/PokemonOptions";
+
+export default {
+  name: "PokemonPage",
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+};
+</script>
